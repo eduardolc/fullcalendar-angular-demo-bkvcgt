c840a81e94f896209e9c69eed5424189aeaf7998
eduardolc <eduardolimacruz2@gmail.com>
2020-10-13 09:13:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fullcalendar-angular-demo-bkvcgt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/fullcalendar-angular-demo-bkvcgt)
---
+++ src/app/app.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin, { Draggable } from '@fullcalendar/interaction';
+import { FullCalendarComponent } from '@fullcalendar/angular';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid';
+import esLocale from '@fullcalendar/core/locales/es';
+import frLocale from '@fullcalendar/core/locales/fr';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  options: any;
+  eventsModel: any;
+  @ViewChild('fullcalendar') fullcalendar: FullCalendarComponent;
+  @ViewChild('external') external: ElementRef;
+  ngOnInit() {
+    // Don't use FullcalendarOption interface
+    this.options = {
+      editable: true,
+      customButtons: {
+        myCustomButton: {
+          text: 'custom!',
+          click: function () {
+            alert('clicked the custom button!');
+          }
+        }
+      },
+      theme: 'standart', // default view, may be bootstrap
+      header: {
+        left: 'prev,next today myCustomButton',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+      columnHeaderHtml: () => {
+          return '<b>Friday!</b>';
+      },
+      locales: [ esLocale, frLocale ],
+      locale: 'fr',
+      // add other plugins
+      plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin, resourceTimeGridPlugin]
+    };
+    new Draggable(this.external.nativeElement, {
+      itemSelector: '.fc-event',
+      eventData: function(eventEl) {
+        return {
+          title: eventEl.innerText
+        };
+      }
+  });
+  }
+  eventClick(model) {
+    console.log(model);
+  }
+  eventDragStop(model) {
+    console.log(model);
+  }
+  dateClick(model) {
+    console.log(model);
+  }
+  updateHeader() {
+    this.options.header = {
+      left: 'prev,next myCustomButton',
+      center: 'title',
+      right: ''
+    };
+  }
+  updateEvents() {
+    this.eventsModel = [{
+      title: 'Updaten Event',
+      start: this.yearMonth + '-08',
+      end: this.yearMonth + '-10'
+    }];
+  }
+  get yearMonth(): string {
+    const dateObj = new Date();
+    return dateObj.getUTCFullYear() + '-' + (dateObj.getUTCMonth() + 1);
+  }
+  dayRender(ev){
+    ev.el.addEventListener('dblclick', () => {
+       alert('double click!');
+    });
+  }
+}
